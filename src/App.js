d671b5652ecc4a117e198688e4191bca2d57e26a
Pedro Marchetti Casteli <pedro.casteli@zaffarinet.com.br>
2021-12-02 11:19:02 -0300

    Adiçao de formulario

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./Components/Expenses/Expenses";
+import NewExpense from "./Components/NewExpense/NewExpense";
 
 function App() {
     const expenses = [
@@ -29,6 +30,7 @@ function App() {
     ];
     return (
         <div>
+            <NewExpense />
             <Expenses expenses={expenses} />
         </div>
     );
